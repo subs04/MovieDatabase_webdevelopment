aa196a3a33e1ace88624c727bb8b2517b592fe4a
Subs <subarnat62@gmail.com>
2026-07-06 10:25:30 +0545

    Navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Movie App</h2>
+
+      <ul>
+        <li>Browse</li>
+        <li>Watchlist</li>
+        <li>Add Movie</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
